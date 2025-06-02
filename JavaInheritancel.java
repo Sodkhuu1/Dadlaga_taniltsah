79c41e5231cd06b91dd16aa358b949fe27db11a4
Sodkhuu1 <bsodkhuu0604@gmail.com>
2025-06-02 22:00:52 +0800

    class ner soliv

--- JavaInheritancel.java
+++ JavaInheritancel.java
@@ -16,7 +16,7 @@ import java.util.*;
             System.out.println("I am singing");
         }
     }
-public class Solution {
+public class JavaInheritancel {
     
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
